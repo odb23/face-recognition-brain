fdd79d7c5e461dd951b41b75e2d93b3b8bbc8457
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-02-11 03:43:55 +0100

    adding Logo

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Tilt from 'react-parallax-tilt';
+import brain from './brain.png'
+import './Logo.css'
+
+const Logo = () => {
+    return (
+        <div classname='ma4 mt0'>
+            <Tilt className='Tilt br2 shadow-2' style={{height: '75px',width: '75px',}}>
+                <div className='Tilt-inner' style={{}}>
+                    <img src={brain} alt='logo'/>
+                </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
