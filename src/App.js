af7abdf3b1e2156f78f5546cab4e43478c07ef7d
Odulaja Oluwadamilare O <damijustice1@gmail.com>
2022-02-12 01:19:31 +0100

    adding background particles

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Particles from "react-tsparticles";
+import {options} from './options.js';
 import './App.css';
 
+
+
 function App() {
+  const particlesInit = (main) => {
+    console.log(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
   return (
     <div className="App">
+      <Particles id="tsparticles"  init={particlesInit} loaded={particlesLoaded} options={options}/>
       <Navigation />
       <Logo />
       <Rank />
